51e9463cdf8240dbd0a948e46cc438dd8a9b9b45
rromero123102 <rromero21332@columbushs.com>
2021-02-11 20:02:36 -0500

    first route setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () =>{
+    return(
+        <div>
+            <h3>Helloo</h3>
+            <small>Main</small>
+        </div>
+    );
+}
+
+export default MainPage;
